e3d8b20915284b1c9ebeeb522f067149683b1182
Ricardo <rikardomoura1@gmail.com>
2020-06-24 14:06:40 -0300

    Order entity | User-order and order-user associations

--- src/main/java/com/project/practice/resources/UserResource.java
+++ src/main/java/com/project/practice/resources/UserResource.java
@@ -1,23 +1,39 @@
 package com.project.practice.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.practice.entities.User;
+import com.project.practice.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService us;
+	
 	@GetMapping
-	public ResponseEntity<User> test(){
+	public ResponseEntity<List<User>> getAll(){
 		
-		User user = new User(1L, "Fake", "fake.name@gmail.com", "91111111", "123456");
+		List<User> users = us.findAll();
 		
-		return ResponseEntity.ok().body(user);
+		return ResponseEntity.ok().body(users);
 		
 	}
 
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User user = us.findById(id);
+		
+		return ResponseEntity.ok().body(user);
+	}
 }
---
+++ src/main/java/com/project/practice/services/UserService.java
@@ -0,0 +1,29 @@
+package com.project.practice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.practice.entities.User;
+import com.project.practice.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository ur;
+	
+	
+	public List<User> findAll(){
+		
+		return ur.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = ur.findById(id);
+		return user.get();
+		
+	}
+}
