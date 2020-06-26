330e9b3edd1e17216c41a069678c524af64b7849
Ricardo <rikardomoura1@gmail.com>
2020-06-25 21:10:10 -0300

    Product and category associations with JoinTable

--- src/main/java/com/project/practice/config/TestConfig.java
+++ src/main/java/com/project/practice/config/TestConfig.java
@@ -50,6 +50,15 @@ public class TestConfig implements CommandLineRunner {
 		cr.saveAll(Arrays.asList(cat1,cat2,cat3));
 		pr.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		pr.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 		User u1 = new User(null,"Bryan","bryan@gmail.com","1111111","12345");
 		User u2 = new User(null,"Kenny","kenny@gmail.com","2222222","123456");
 		
