85c0c7330a392d8c4af76de51a2551f0d289e166
Ricardo <rikardomoura1@gmail.com>
2020-06-27 13:09:04 -0300

    Payment Entity

--- src/main/java/com/project/practice/config/TestConfig.java
+++ src/main/java/com/project/practice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.project.practice.entities.Category;
 import com.project.practice.entities.Order;
 import com.project.practice.entities.OrderItem;
+import com.project.practice.entities.Payment;
 import com.project.practice.entities.Product;
 import com.project.practice.entities.User;
 import com.project.practice.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		oir.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment payment1 = new Payment(null, Instant.parse("2020-06-24T16:30:12Z"), o1);
+		o1.setPayment(payment1);
+		or.save(o1);
 		
 	}
 
