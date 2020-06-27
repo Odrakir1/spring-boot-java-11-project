5164126e3330bd55ac0170f9ddb2f2945e1f78f1
Ricardo <rikardomoura1@gmail.com>
2020-06-27 16:59:33 -0300

    FindById exception handling

--- src/main/java/com/project/practice/services/UserService.java
+++ src/main/java/com/project/practice/services/UserService.java
@@ -8,44 +8,43 @@ import org.springframework.stereotype.Service;
 
 import com.project.practice.entities.User;
 import com.project.practice.repositories.UserRepository;
+import com.project.practice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository ur;
-	
-	
-	public List<User> findAll(){
-		
+
+	public List<User> findAll() {
+
 		return ur.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> user = ur.findById(id);
-		return user.get();
+		return user.orElseThrow(()->new ResourceNotFoundException(id));
 		
 	}
-	
+
 	public User insert(User user) {
 		return ur.save(user);
 	}
-	
+
 	public void delete(Long id) {
 		ur.deleteById(id);
 	}
-	
-	
-	public User update(Long id,User user) {
+
+	public User update(Long id, User user) {
 		User entity = ur.getOne(id);
-		updateUser(entity,user);
-		return ur.save(entity) ;
+		updateUser(entity, user);
+		return ur.save(entity);
 	}
 
 	private void updateUser(User entity, User user) {
 		entity.setName(user.getName());
 		entity.setFone(user.getFone());
 		entity.setEmail(user.getEmail());
-		
+
 	}
 }
