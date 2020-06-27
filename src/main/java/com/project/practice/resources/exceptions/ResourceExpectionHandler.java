db0aba17c59926f7a3b5cbc1d96abc72adb63c9b
Ricardo <rikardomoura1@gmail.com>
2020-06-27 18:07:16 -0300

    Fixed User findById exception | Added User delete exception handling

--- src/main/java/com/project/practice/resources/exceptions/ResourceExpectionHandler.java
+++ src/main/java/com/project/practice/resources/exceptions/ResourceExpectionHandler.java
@@ -7,12 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.project.practice.services.exceptions.DatabaseException;
 import com.project.practice.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExpectionHandler {
 
+	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> notFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
@@ -21,4 +24,16 @@ public class ResourceExpectionHandler {
 		
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
 }
--- src/main/java/com/project/practice/services/UserService.java
+++ src/main/java/com/project/practice/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.project.practice.entities.User;
 import com.project.practice.repositories.UserRepository;
+import com.project.practice.services.exceptions.DatabaseException;
 import com.project.practice.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,7 +35,16 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		ur.deleteById(id);
+		try {
+			ur.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
+		
 	}
 
 	public User update(Long id, User user) {
